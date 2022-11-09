28d151f64c8cd286d82d830426ef0b5430475ebf
bds01088 <bds01088@naver.com>
2022-11-09 16:25:52 +0900

    spark 없앰

--- backend/src/main/java/bigdataproject/backend/api/controller/SparkController.java
+++ backend/src/main/java/bigdataproject/backend/api/controller/SparkController.java
@@ -1,4 +1,4 @@
-package bigdataproject.backend.api.controller;
+//package bigdataproject.backend.api.controller;
 
 import bigdataproject.backend.api.service.SparkService;
 import bigdataproject.backend.db.WebClient.WordCountDTO;
@@ -10,19 +10,19 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
 
-@RestController
-@RequestMapping("/spark")
-@CrossOrigin
-public class SparkController {
-
-    @Autowired
-    SparkService sparkService;
-
-    @GetMapping
-    @ApiOperation(value = "지난 시간 거래 트래픽 조회", notes = "input : int (ex. 10, 30, 60) output : Map 형식 {\"KRW-BTC\" : 29, ...}")
-    public List<WordCountDTO> count(@RequestParam(required = true) int time) {
-        LocalDateTime ldt = LocalDateTime.now(ZoneId.of("UTC")).minusMinutes(time);
-        LocalDateTime now = LocalDateTime.now();
-        return sparkService.getCount(ldt, now, time/10);
-    }
-}
+//@RestController
+//@RequestMapping("/spark")
+//@CrossOrigin
+//public class SparkController {
+//
+//    @Autowired
+//    SparkService sparkService;
+//
+//    @GetMapping
+//    @ApiOperation(value = "지난 시간 거래 트래픽 조회", notes = "input : int (ex. 10, 30, 60) output : Map 형식 {\"KRW-BTC\" : 29, ...}")
+//    public List<WordCountDTO> count(@RequestParam(required = true) int time) {
+//        LocalDateTime ldt = LocalDateTime.now(ZoneId.of("UTC")).minusMinutes(time);
+//        LocalDateTime now = LocalDateTime.now();
+//        return sparkService.getCount(ldt, now, time/10);
+//    }
+//}
